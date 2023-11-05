66a8c66373c870099b0519e12569f6118308cee9
Hendrik Käo <hendrik.kao@gmail.com>
2023-11-05 18:28:16 +0100

    Improve text wrapping in `DescriptionList`

--- src/components/DescriptionList/DescriptionList.tsx
+++ src/components/DescriptionList/DescriptionList.tsx
@@ -5,9 +5,11 @@ interface DescriptionListProps {
 export const DescriptionList = ({ items }: DescriptionListProps) => (
   <dl className="divide-y divide-zinc-900/20">
     {items.map(([term, description]) => (
-      <div key={term} className="py-6 grid grid-cols-2 gap-2">
-        <dt className="text-sm font-semibold text-zinc-900">{term}</dt>
-        <dd className="text-sm text-zinc-900 col-span-1">{description}</dd>
+      <div key={term} className="py-6 grid grid-cols-2 gap-4">
+        <dt className="text-sm font-semibold text-zinc-900 truncate">{term}</dt>
+        <dd className="text-sm text-zinc-900 col-span-1 truncate">
+          {description}
+        </dd>
       </div>
     ))}
   </dl>

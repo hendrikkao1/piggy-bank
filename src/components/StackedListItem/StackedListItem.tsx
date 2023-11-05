0f2a7ce21fade4ddcdb62ac3c85a6d737e80291d
Hendrik Käo <hendrik.kao@gmail.com>
2023-11-05 18:25:46 +0100

    Improve text wrapping in `StackedListItem`

--- src/components/StackedListItem/StackedListItem.tsx
+++ src/components/StackedListItem/StackedListItem.tsx
@@ -11,17 +11,15 @@ export const StackedListItem = ({
   subTitle,
   title,
 }: StackedListItemProps) => (
-  <section className="flex justify-between gap-x-4 py-6">
-    <div className="flex min-w-0 gap-x-4">
-      <div className="min-w-0 flex flex-col gap-1">
-        <p className="text-sm font-semibold text-zinc-900">{title}</p>
-        <p className="text-xs truncate text-zinc-600">{description}</p>
-      </div>
+  <section className="grid grid-cols-2 gap-4 py-6">
+    <div className="flex flex-col gap-1 overflow-hidden">
+      <p className="text-sm truncate font-semibold text-zinc-900">{title}</p>
+      <p className="text-xs truncate text-zinc-600">{description}</p>
     </div>
-    <div className="flex items-center gap-4">
-      <div className="shrink-0 flex flex-col items-end gap-1">
-        <p className="text-sm text-zinc-900">{subTitle}</p>
-        <p className="text-xs text-zinc-600">{meta}</p>
+    <div className="flex items-center gap-4 justify-end">
+      <div className="flex flex-col gap-1 overflow-hidden">
+        <p className="text-sm truncate text-zinc-900">{subTitle}</p>
+        <p className="text-xs truncate text-zinc-600 self-end">{meta}</p>
       </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
